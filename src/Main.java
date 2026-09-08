179ac775e99af095b03362940841c60b78cb1b07
mateustavaresw <mateustavareszx007@gmail.com>
2026-09-08 14:02:22 -0300

    Atribuindo valores

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args){
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        float notaDoFilme = 8.0f;
+        System.out.println("Esse é o filmtrack");
+        System.out.println("Filme: Top Gun: Maverick");
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Nota do filme: " + notaDoFilme);
+    }
+}
